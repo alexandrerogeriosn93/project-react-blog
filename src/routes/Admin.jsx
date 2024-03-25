89353141a16692549be97622d8fc68e23aa27a15
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-25 03:59:07 -0300

    feat: add create component Admin

---
+++ src/routes/Admin.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import blogFetch from "../axios/config";
+
+import "./Admin.css";
+
+const Admin = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="admin">
+      <h1>Gerenciar Posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <div className="actions">
+              <Link className="btn edit-btn">Editar</Link>
+              <Link className="btn delete-btn">Excluir</Link>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Admin;
