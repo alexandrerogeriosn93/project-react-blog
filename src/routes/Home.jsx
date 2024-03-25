7f993c279ca6adf2a207b4430165aafe26378197
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-25 01:59:36 -0300

    feat: access posts data from api https://jsonplaceholder.typicode.com

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+import "./Home.css";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = response.data;
+
+      console.log(data);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return <div>Home</div>;
 };
 
